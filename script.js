277a55bd4735ac9b21efcfb1ee1323a41abd7fc2
Cedric Nicolas <cedricnicolas@Cedrics-MacBook-Pro.local>
2017-12-26 16:23:11 -0500

    Updated

--- script.js
+++ script.js
@@ -1,1 +1,7 @@
-
+var dropdown = document.getElementsByClassName("collapse navbar-collapse");
+var dropButton = document.getElementById("dropButton");
+dropButton.addEventListener("click", function() {
+  for (var i = 0; i < dropdown.length; i++) {
+    dropdown[i].classList.toggle("dropdownClass");
+  }
+})
