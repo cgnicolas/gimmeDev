7b75b3b8bb96f557fb9c5a55235ae69884b01525
Cedric Nicolas <cedricnicolas@Cedrics-MacBook-Pro.local>
2017-12-26 16:27:36 -0500

    Remove JS

--- script.js
+++
@@ -1,7 +0,0 @@
-var dropdown = document.getElementsByClassName("collapse navbar-collapse");
-var dropButton = document.getElementById("dropButton");
-dropButton.addEventListener("click", function() {
-  for (var i = 0; i < dropdown.length; i++) {
-    dropdown[i].classList.toggle("dropdownClass");
-  }
-})
